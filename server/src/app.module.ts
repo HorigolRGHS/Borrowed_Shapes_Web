c67cc3b5ac05547e869fb91b51a806be2ca5241a
Shile <dangvtn2004@gmail.com>
2026-05-30 13:49:57 +0700

    Fix: Fix the missing character error after renaming the folder.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,7 +7,7 @@ import { AuthModule } from './auth/auth.module';
 import { SessionsModule } from './sessions/sessions.module';
 import { PresenceModule } from './presence/presence.module';
 import { GameModule } from './game/game.module';
-import { AchievementModule } from './achievement/achievement.module';
+import { AchievementModule } from './achievements/achievements.module';
 import { I18nModule } from './common/i18n/i18n.module';
 import { HeartbeatInterceptor } from './common/interceptors/heartbeat.interceptor';
 import { WikiModule } from './wiki/wiki.module';
@@ -33,4 +33,4 @@ import mikroOrmConfig from './mikro-orm.config';
     },
   ],
 })
-export class AppModule {}
+export class AppModule { }
